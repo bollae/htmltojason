2ef888c3ad093e37686ecbe4cb1b184fa93cf898
Eszter Bolla <bolla.eszter@gmail.com>
2019-09-16 21:15:31 +0000

    Arguments, and html validation

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 #!/user/bin/env python
 
-import os, sys, urllib.request
+import os, sys, urllib.request, argparse
 
-url = sys.argv[1]
-if len (sys.argv) != 2 :
-    sys.exit ("Usage: html_to_jsno <url>")
-
+parser = argparse.ArgumentParser(description='HTML to JSON')
+parser.add_argument('url', type=str, help='Input url to download html file')
+args = parser.parse_args()
 
 print("Checking internet connection...")
 if urllib.request.urlopen("http://google.com").getcode() == 200:
     print("Network connection ok")
 else:
-    sys.exit("Network connection failed")
+    sys.exit("Network connection failed, exit")
+
+print(f"Input html file: {args.url}")
+response = urllib.request.urlopen(args.url)
+
+if response.headers['content-type'] == 'text/html; charset=UTF-8':
+    print(f"{args.url} is a valid html file, continue process...")
+else:
+    sys.exit("This is not valid html, exit")
+
