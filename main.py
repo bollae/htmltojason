465c6f4b224f14fb21294924cc75b47678b1f7c3
Eszter Bolla <bolla.eszter@gmail.com>
2019-09-17 20:32:36 +0000

    find the rss fedd link in html docs

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/user/bin/env python
 
-import os, sys, urllib.request, argparse
+import os, sys, urllib.request, argparse, json
+from bs4 import BeautifulSoup
+#from pprint import pprint
 
 parser = argparse.ArgumentParser(description='HTML to JSON')
 parser.add_argument('url', type=str, help='Input url to download html file')
@@ -19,4 +21,16 @@ if response.headers['content-type'] == 'text/html; charset=UTF-8':
     print(f"{args.url} is a valid html file, continue process...")
 else:
     sys.exit("This is not valid html, exit")
+
+soup = BeautifulSoup(response, 'html.parser')
+
+feeds = soup.find_all('link', attrs={"type": "application/rss+xml"})
+print(str(feeds.get('href')))
+type(feeds)
+
+dic = {'rss' : [], 'atom' :  []}
+print(dic)
+json = json.dumps(dic)
+print(json)
+
 
